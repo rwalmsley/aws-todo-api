459fd0bee5374cd63cbd783579cef71aa0fe97ec
Ryan Walmsley <ryan.walmsley87@gmail.com>
2018-09-11 10:51:02 -0700

    Update table AWS config for test env

--- app/table.js
+++ app/table.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 var AWS = require('aws-sdk');
+const isDev = process.env.NODE_ENV !== 'production';
 
+const localConfig = {
+    region: process.env.DB_REGION,
+    endpoint: `http://localhost:8000`,
+    accessKeyId: process.env.DB_USER,
+    secretAccessKey: process.env.DB_PASS
+};
 
-AWS.config.update({
+const remoteConfig = {
     region: process.env.DB_REGION,
     endpoint: process.env.DB_HOST,
     accessKeyId: process.env.DB_USER,
     secretAccessKey: process.env.DB_PASS
-});
+}
+
+AWS.config.update(isDev ? localConfig : remoteConfig);
 
 var dynamodb = new AWS.DynamoDB();
 
